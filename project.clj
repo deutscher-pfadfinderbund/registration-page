31f25c29e26a6bf51d8027cffcd2a025cf3e407a
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-11-28 00:03:50 +0100

    Initial

---
+++ project.clj
@@ -0,0 +1,31 @@
+(defproject ksr "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
+                 [org.clojure/test.check "0.9.0"]
+
+                 [hiccup "2.0.0-alpha1"]
+
+                 [io.pedestal/pedestal.service "0.5.3"]
+
+                 [clj-http "3.7.0"]
+                 [org.clojure/data.json "0.2.6"]
+
+                 [io.pedestal/pedestal.jetty "0.5.3"]
+                 [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.22"]
+                 [org.slf4j/jcl-over-slf4j "1.7.22"]
+                 [org.slf4j/log4j-over-slf4j "1.7.22"]]
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  ;; If you use HTTP/2 or ALPN, use the java-agent to pull in the correct alpn-boot dependency
+  ;:java-agents [[org.mortbay.jetty.alpn/jetty-alpn-agent "2.0.5"]]
+  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "ksr.server/run-dev"]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.3"]]}
+             :uberjar {:aot [ksr.server]}}
+  :repl-options {:init (do (require 'ksr.server)
+                           (def the-server (ksr.server/run-dev)))}
+  :main ^{:skip-aot true} ksr.server)
+
---
+++ src/ksr/service.clj
@@ -0,0 +1,92 @@
+(ns ksr.service
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :as body-params]
+            [ring.util.response :as ring-resp]
+            [hiccup.page :as hp]))
+
+(defn register-page [request]
+  (ring-resp/response
+   (hp/html5
+    (hp/include-css "css/bootstrap.min.css")
+    (hp/include-css "css/main.css")
+    [:div.container
+     [:div.row
+      [:div.col-md-1]
+      [:div.col-md-10.col-sm-10.card.card-3
+       [:div.row
+        [:div.col-1]
+        [:div.col-10
+         [:img {:src "img/schriftzug-dpb.svg"
+                :style {:width "100%"}}]]]
+       [:h4.text-center {:style {:padding-top 0}} "Anmeldung zum"]
+       [:h1.text-center "Knappen-Späher-Ritter Lager"]
+       [:h4.text-center {:style {:padding-top 0}} "im sonnigen Remscheid"]
+       [:br]
+       [:div.text-center "Es sind nur noch " [:strong (+ 10 (rand-int 20))] " Plätze verfügbar!"]
+       [:br]
+       [:form {:action "/auth" :method :POST}
+        [:div.form-group
+         [:label "Name *"]
+         [:input.form-control {:name "name" :required true}]]
+        [:div.form-group
+         [:label "Einheit *"]
+         [:input.form-control {:name "einheit" :required true}]]
+        [:div.form-group
+         [:label "Stand *"]
+         [:select.form-control
+          [:option {:value "Jungwolf"} "Jungwolf"]
+          [:option {:value "Knappe"} "Knappe"]
+          [:option {:value "Späher"} "Späher"]
+          [:option {:value "St.-Georgs-Knappe"} "St.-Georgs-Knappe"]
+          [:option {:value "Ordensritter"} "Ordensritter"]
+          [:option {:value "St.-Georgs-Ritter"} "St.-Georgs-Ritter"]
+          [:option {:value "Sonstiges"} "Sonstiges"]]]
+        [:div.form-group
+         [:label "Essensbesonderheiten"]
+         [:input.form-control {:name "essen-besonderheiten"}]]
+        [:div.form-group
+         [:label "Das letzte Thema in meinem Ständekreis war..."]
+         [:textarea.form-control {:name "das-letzte-thema-staendekreis" :rows 3}]]
+        [:div.form-group
+         [:label "Orden ist für mich..."]
+         [:textarea.form-control {:name "orden-ist-fuer-mich" :rows 3}]]
+        [:input {:class "btn btn-primary"
+                 :type :submit
+                 :value "Anmelden"}]]
+
+       [:hr {:style {:margin-top "3rem"}}]
+       [:h2 "Wichtige Informationen"]
+       [:br]
+       [:a.btn.btn-light {:href "pdf/einladung-ksr.pdf"}
+        "Einladung als PDF"]
+
+       [:h4 "Anreise"]
+       [:p "Adresse: Hammertal 4, 42857 Remscheid"]
+       [:p "Der Bahnhof \"Remscheid-Güldenwerth\" ist ca. 10 Minuten fußläufig über einen Wanderweg erreichbar."]
+       [:p "Bei Anreise mit dem " [:strong "Auto"] " parkt ihr bitte dem P&R Parkplatz am Bahnhof Güldenwerth und wandert die paar Meter zum Kotten."]
+       [:div.myIframe
+        [:iframe {:src "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2501.8611792782926!2d7.165978715930733!3d51.166348643402436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47b92a86dce4928d%3A0x94cced3261c0deb0!2sDiedrichs-Kotten!5e0!3m2!1sde!2sde!4v1511819351039"
+                  :frameborder 0
+                  :allowfullscreen true
+                  :style {:border 0}}]]
+
+       [:h4 "Shuttle"]
+       [:p "Solltet ihr Shuttlebedarf haben, so meldet euch unter "
+        [:a {:href "mailto:shuttle@dpb-remscheid.de"} "shuttle@dpb-remscheid.de"]]
+
+
+       ]]])))
+
+(def common-interceptors [(body-params/body-params) http/html-body])
+
+(def routes #{["/" :get (conj common-interceptors `register-page)]})
+
+(def service {:env :prod
+              ::http/routes routes
+              ::http/resource-path "/public"
+              ::http/type :jetty
+              ::http/port 8080
+              ::http/container-options {:h2c? true
+                                        :h2? false
+                                        :ssl? false}})
+
